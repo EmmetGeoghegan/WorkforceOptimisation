9a61132f2675c2631c852a2fe7c82a018d97e457
Emmet Geoghegan <46162302+EmmetGeoghegan@users.noreply.github.com>
2019-08-12 11:58:37 +0100

    First Working Solution
    TODO:
    Move funcs to funcs.py
    
    Make a better distribution Algorithm

--- wfo.py
+++ wfo.py
@@ -78,12 +78,20 @@ Peopletobeassigned = People_Objects
 Jobstobeassigned = Job_Objects
 
 
+def fitnessfunc(People):
+    totaltime = 0
+    for i in People:
+        totaltime += abs(i.remainingtime)
+    return totaltime
+
+
 def spreadJobs(People, Jobs):
     while Jobs != []:
         rand_person = random.choice(People)
         rand_job = random.choice(Jobs)
         rand_person.addjob(rand_job)
         Jobs.remove(rand_job)
+
 
 def gettimes(person_list):
     timelist = []
@@ -93,12 +101,13 @@ def gettimes(person_list):
     return index_min
         # Look at test func
 
+
 def stripjobs(Person):
-    print(Person.name, [x.name for x in Person.jobs])
+    # print(Person.name, [x.name for x in Person.jobs])
     jobs = []
     jobs += Person.jobs
     for j in Person.jobs[:]:
-        i.removejob(j)
+        Person.removejob(j)
     return jobs
 
 # # Do Our first assign
@@ -110,15 +119,21 @@ def stripjobs(Person):
 #     Job_Objects.remove(rand_job)
 print("-------- Spawn ------------")
 spreadJobs(Peopletobeassigned, Jobstobeassigned)
-
-
-for rounds in range(1,200,1):
+bestscore = 100000
+for rounds in range(1,200000,1):
     print("      ")
     print(f"ROUND {rounds}")
     print("      ")
     Peopletobeassigned = []
+    configscore = fitnessfunc(People_Objects)
+    if configscore < bestscore:
+        bestconfig = []
+        bestscore = configscore
+        for i in People_Objects:
+            bestconfig.append([i.name, i.time, i.remainingtime, [x.name for x in i.jobs]])
+
     for i in People_Objects:
-        print(i.name, i.time, i.remainingtime, [x.name for x in i.jobs])
+        # print(i.name, i.time, i.remainingtime, [x.name for x in i.jobs])
         if i.remainingtime < 0:
             Peopletobeassigned.append(i)
             Jobstobeassigned += i.jobs
@@ -126,44 +141,49 @@ for rounds in range(1,200,1):
                 i.removejob(j)
 
 
-    print("      ")
-    print(f"After Checking")
-    print("      ")
-    print("--- Jobless People ---")
-    print([x.name for x in Peopletobeassigned])
-    print("----------------------")
-    print("      ")
-    print("--- Peopleless Jobs ---")
-    print([x.name for x in Jobstobeassigned])
-    print("-----------------------")
-    print("   ")
+    # print("      ")
+    # print(f"After Checking")
+    # print("      ")
+    # print("--- Jobless People ---")
+    # print([x.name for x in Peopletobeassigned])
+    # print("----------------------")
+    # print("      ")
+    # print("--- Peopleless Jobs ---")
+    # print([x.name for x in Jobstobeassigned])
+    # print("-----------------------")
+    # print("   ")
+    #
+    # print("------- After Removal --------")
+    # for i in People_Objects:
+    #     print(i.name, i.remainingtime, [x.name for x in i.jobs])
+    # print("  ")
+    #
+    # print(f"The Lowest person is {gettimes(People_Objects)+1}")
+    weakestlink = gettimes(People_Objects)
+    # print("BEFORE", [x.name for x in Jobstobeassigned])
+    Jobstobeassigned += stripjobs(People_Objects[weakestlink])
+    # print("AFTER", [x.name for x in Jobstobeassigned])
 
-    print("------- After Removal --------")
-    for i in People_Objects:
-        print(i.name, i.remainingtime, [x.name for x in i.jobs])
-    print("  ")
-
-    print(f"The Lowest person is {gettimes(People_Objects)}")
-    print("BEFORE", [x.name for x in Jobstobeassigned])
-    Jobstobeassigned += stripjobs(People_Objects[gettimes(People_Objects)])
-    print("AFTER", [x.name for x in Jobstobeassigned])
-
-    Peopletobeassigned += People_Objects[gettimes(People_Objects)]
-    print("--- Jobless People ---")
-    print([x.name for x in Peopletobeassigned])
-    print("----------------------")
-    print("      ")
-    print("--- Peopleless Jobs ---")
-    print([x.name for x in Jobstobeassigned])
-    print("-----------------------")
+    Peopletobeassigned.append(People_Objects[weakestlink])
+    # print("--- Jobless People ---")
+    # print([x.name for x in Peopletobeassigned])
+    # print("----------------------")
+    # print("      ")
+    # print("--- Peopleless Jobs ---")
+    # print([x.name for x in Jobstobeassigned])
+    # print("-----------------------")
 
     spreadJobs(Peopletobeassigned, Jobstobeassigned)
-    print(" ")
-    print(" ")
+    # print(" ")
+    # print(" ")
 # print("------")
 # for i in People_Objects:
 #     print(i.name, i.time, i.remainingtime, [x.name for x in i.jobs])
 #
+
 # print("--------")
 # print([x.name for x in Job_Objects])
 # print([x.name for x in Peopletobeassigned])
+
+print(f"Best score: {bestscore}")
+print(f"Best Config: \n {bestconfig}")
